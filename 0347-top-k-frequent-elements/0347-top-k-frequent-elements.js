d64961ed4f82a9d392e26f22e1ade717d0159899
Muhammad Askari <91508768+Muhammad32130@users.noreply.github.com>
2024-01-26 01:06:30 -0600

    Time: 66 ms (67.60%), Space: 50.5 MB (15.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,16 +4,10 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-       const map = new Map;
-    for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i])) {
-            var temp = map.get(nums[i]);
-            temp += 1;
-            map.set(nums[i], temp);
-        } else {
-            map.set(nums[i], 1)
-        }
-    }
-    const sortedKeys = Array.from(map.keys()).sort((a,b) => map.get(b) - map.get(a));
-    return sortedKeys.slice(0, k);
+    const m = new Map();
+    nums.forEach(num => {
+        let val = m.get(num) || 0
+        m.set(num, ++val)
+    })
+    return Array.from(m.keys()).sort((a,b)=>m.get(b) - m.get(a)).slice(0, k)
 };
