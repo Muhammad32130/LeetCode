814d3fbaf6cdf98f9d8f318d3ceb31e31788da99
Muhammad Askari <91508768+Muhammad32130@users.noreply.github.com>
2024-01-31 00:29:09 -0600

    Time: 72 ms (30.53%), Space: 50.8 MB (24.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,6 +3,8 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    // Optimal 
+    //the time complexity is O(n), and the memory complexity is O(n).
     s = s.toLowerCase().replace(/[^0-9a-z]/gi, '');
     let r = s.length - 1 
     for (let i = 0; i < Math.floor(s.length / 2); i++ ){
