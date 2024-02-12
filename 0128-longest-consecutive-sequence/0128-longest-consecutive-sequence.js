3e2645ae1df4344bac908432ec2ac3c7740048a2
Muhammad Askari <91508768+Muhammad32130@users.noreply.github.com>
2024-02-11 22:25:01 -0600

    Time: 710 ms (38.85%), Space: 74 MB (19.97%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+   const numSet = new Set(nums);
+    let longestStreak = 0;
+
+    for (let num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+            while (numSet.has(currentNum + 1)) {
+                console.log(currentNum)
+                currentNum++;
+                currentStreak++;
+            }
+
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+};
